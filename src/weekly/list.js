9605a07f9d7aea51b70c32caece7305f059a7b4c
Khawla khaled <20197216@stu.uob.edu.bh>
2026-05-10 19:46:51 +0300

    weekly list.js updated

--- src/weekly/list.js
+++ src/weekly/list.js
@@ -13,7 +13,7 @@
 
 // --- Element Selections ---
 // TODO: Select the section for the week list using its id 'week-list-section'.
-
+const weekListSection = document.getElementById('week-list-section');
 // --- Functions ---
 
 /**
@@ -42,7 +42,27 @@
  * the weeks table) so that details.js can read the id from the URL.
  */
 function createWeekArticle(week) {
-  // ... your implementation here ...
+ const article = document.createElement('article');
+
+  const title = document.createElement('h2');
+  title.textContent = week.title;
+
+  const date = document.createElement('p');
+  date.textContent = "Starts on: " + week.start_date;
+
+  const description = document.createElement('p');
+  description.textContent = week.description;
+
+  const link = document.createElement('a');
+  link.href = "details.html?id=" + week.id;
+  link.textContent = "View Details & Discussion";
+
+  article.appendChild(title);
+  article.appendChild(date);
+  article.appendChild(description);
+  article.appendChild(link);
+
+  return article;
 }
 
 /**
@@ -59,7 +79,15 @@ function createWeekArticle(week) {
  *    - Append the returned <article> to the list section.
  */
 async function loadWeeks() {
-  // ... your implementation here ...
+  const response = await fetch('./api/index.php');
+  const result = await response.json();
+  if (result.success) {
+    weekListSection.innerHTML = "";
+    result.data.forEach(week => {
+      const article = createWeekArticle(week);
+      weekListSection.appendChild(article);
+    });
+  }
 }
 
 // --- Initial Page Load ---
